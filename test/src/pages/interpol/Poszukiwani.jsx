f6efcc5dbd6e87eec3b3fc94b7b03a5fd5f25d06
Bart <bart@gmail.com>
2023-12-25 01:53:51 +0100

    01532512

--- test/src/pages/interpol/Poszukiwani.jsx
+++ test/src/pages/interpol/Poszukiwani.jsx
@@ -13,83 +13,60 @@ export default function Poszukiwani() {
             });
         });
     }, []);
-    console.log(data);
-
     const WantedList = ({ data }) => {
         return (
-            <div className="w-1/2">
+            <div className="w-2/3 md:w-3/4">
                 <h1 className="text-4xl mb-8 text-center">
                     MOST WANTED BY INTERPOL<span className="text-2xl">sl</span>
                 </h1>
-
-                <div className="grid grid-cols-2 gap-x-20">
+                <div className="mt-6 grid grid-cols-1 gap-x-20 gap-y-20 md:grid-cols-2">
                     {Object.keys(data).map((key) => {
                         const person = data[key];
                         return (
-                            <div key={key} className="flex my-5">
-                                <div className="flex flex-col">
-                                    <div className="flex justify-between h-60">
-                                        <div className="w-1/2">
-                                            <h2>IMIĘ: {person.name}</h2>
-                                            <p>WIEK: {person.age}</p>
-                                            <p>PSEUDONIM: {person.nickname}</p><br />
-                                            {person.groups &&
-                                                person.groups.length > 0 && (
-                                                    <>
-                                                        <h3>
-                                                            NIELEGALNA
-                                                            ORGANIZACJA:
-                                                        </h3>
-                                                        {person.groups &&
-                                                            person.groups
-                                                                .length > 0 && (
-                                                                <>
-                                                                    <ul>
-                                                                        {person.groups.map(
-                                                                            (
-                                                                                group,
-                                                                                index
-                                                                            ) => (
-                                                                                <li
-                                                                                    key={
-                                                                                        index
-                                                                                    }
-                                                                                >
-                                                                                    {
-                                                                                        group.name
-                                                                                    }
-                                                                                </li>
-                                                                            )
-                                                                        )}
-                                                                    </ul>
-                                                                </>
-                                                            )}
-                                                    </>
-                                                )}
-                                        </div>
-
-                                        <div className="w-1/3">
-                                            <img
-                                                src={person.image}
-                                                alt={`Wanted: ${person.name}`}
-                                            />
-                                        </div>
+                            <div className="flex flex-col" key={key}>
+                                <div className="w-full flex flex-col md:flex-row">
+                                    <div className="w-full md:w-1/3  md:max-h-56 md:order-2 ">
+                                        <img
+                                            className="max-h-56 m-auto md:m-0 md:float-right"
+                                            src={person.image}
+                                            alt={`Wanted: ${person.name}`}
+                                        />
                                     </div>
-                                    <div className="">
-                                        <h3>Wanted For:</h3>
-                                        <ul className="list-disc">
-                                            {person.wantedFor.map(
-                                                (crime, index) => (
-                                                    <li key={index}>{crime}</li>
-                                                )
-                                            )}
-                                        </ul>
+                                    <div className="w-full md:w-2/3">
+                                        <h1>Imię: {person.name}</h1>
+                                        <p>Wiek: {person.age}</p>
+                                        <p>Płeć: {person.sex}</p>
+                                        <p>Pseudonim: {person.nickname}</p>
+                                        {person.groups && person.groups.length > 0 && (
+                                            <div className="mt-6">
+                                                <h3>Członek nielegalnych organizacji:</h3>
+                                                    <ul>
+                                                        {person.groups.map(
+                                                            (group,index) => (
+                                                                <li key={index}>{ group.name}</li>
+                                                            )
+                                                        )}
+                                                    </ul>
+                                            </div>
+                                        )}
+                                                
                                     </div>
+                                    
+                                </div>
+                                <div className="w-full bg-slate-600">
+                                    <h3>Wanted For:</h3>
+                                    <ul className="list-disc">
+                                        {person.wantedFor.map(
+                                            (crime, index) => (
+                                                <li key={index} className="ml-6">{crime}</li>
+                                            )
+                                        )}
+                                    </ul>
                                 </div>
                             </div>
                         );
                     })}
-                </div>
+                </div>                
             </div>
         );
     };
